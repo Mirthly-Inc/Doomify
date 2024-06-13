a985b37a5e6d24e438fa46b787c799da3228df2b
Anand Prabhu <anandprabhu530@gmail.com>
2024-06-13 20:06:11 +0530

    Signup page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,16 +1,22 @@
+"use client";
 const Login = () => {
+  const handleoauthclick = () => {
+    console.log("Clicked With O-auth");
+  };
   return (
     <div className="border-2 border-white flex flex-col items-center lg:w-[75%] lg:mx-auto">
       <div className="flex items-center justify-center p-6 border-2 border-red-600 w-full">
         <div>Navbar will be here</div>
       </div>
-      <div className="pt-20 w-full flex justify-center items-center flex-col">
-        <div className="text-4xl font-semibold pb-4">Create an Account</div>
-        <div className="text-zinc-400 text-lg">
+      <div className="pt-20 flex items-center justify-center flex-col">
+        <div className="lg:text-4xl text-3xl font-semibold pb-4">
+          Create an Account
+        </div>
+        <div className="text-zinc-400 lg:text-lg text-md text-center">
           Learn Efficiently by competing with others using Generative AI.
         </div>
       </div>
-      <div className="pt-10 w-full border-2 border-green-400 grid grid-cols-7">
+      <div className="pt-10 lg:w-full md:w-full border-2 border-green-400 grid grid-cols-1 lg:grid-cols-7 md:grid-cols-7 w-[70%]">
         <div></div>
         <div className="col-span-2">
           <form className="flex flex-col gap-4">
@@ -25,7 +31,7 @@ const Login = () => {
               className="bg-zinc-900 outline-none p-2 text-white rounded-md placeholder:text-zinc-400"
             />
             <button className="border-2 border-white p-2 rounded-md cursor-pointer text-sm flex justify-between items-center">
-              <div>Create an Account</div>
+              <div>Create a new Account</div>
               <div>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -46,8 +52,25 @@ const Login = () => {
           </form>
         </div>
         <div className="flex justify-center items-center">\</div>
-        <div className="border-2 border-blue-500 col-span-2">
-          Sign In With Google/Github
+        <div className="col-span-2 flex flex-col gap-4">
+          <button
+            onClick={handleoauthclick}
+            className="border-white border-2 p-2 rounded-md w-full"
+          >
+            Sign In With Google
+          </button>
+          <button
+            onClick={handleoauthclick}
+            className="border-white border-2 p-2 rounded-md w-full"
+          >
+            Sign In With Github
+          </button>
+          <button
+            onClick={handleoauthclick}
+            className="border-white border-2 p-2 rounded-md w-full"
+          >
+            Sign In With Facebook
+          </button>
         </div>
       </div>
     </div>
